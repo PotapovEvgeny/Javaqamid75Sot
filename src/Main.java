5cffa659c7a5bb3c1e7019a8fb0b6ea6edbc8d1c
Evgen7985 <Evgen7985@yandex.ru>
2023-11-04 17:17:21 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int score = 300;
+        int depositAccount = 2000;
+
+        int bonus;
+
+        if (depositAccount > 1000) {
+            bonus = depositAccount / 100;
+        } else {
+            bonus = 0;
+        }
+
+
+
+    int balance = score + depositAccount + bonus;
+
+        System.out.println(" Итоговый счет: " + balance + " рублей. ");
+        System.out.println(" Ваш бонус: " + bonus + " рублей. ");
+}
+}
